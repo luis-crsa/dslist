dfd01e72630acd6132dc41b2226d356fef777c14
Luis <luiscrsarmento@gmail.com>
2024-11-11 22:51:30 -0300

    Game seed

--- src/main/java/com/project/dslist/entities/Game.java
+++ src/main/java/com/project/dslist/entities/Game.java
@@ -10,30 +10,38 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
+
     @Column(name = "game_year")
     private Integer year;
+    
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){
     
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score ,String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -71,13 +79,21 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }   
 
     public String getImgUrl() {
         return imgUrl;
@@ -126,6 +142,6 @@ public class Game {
         } else if (!id.equals(other.id))
             return false;
         return true;
-    }    
+    } 
 
 }
