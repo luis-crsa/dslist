83d95ac2c26e76ec75a539bed7896415fb4a0379
Luis <luiscrsarmento@gmail.com>
2024-11-12 20:51:51 -0300

    GET games by list

--- src/main/java/com/project/dslist/dto/GameMinDTO.java
+++ src/main/java/com/project/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.project.dslist.dto;
 
 import com.project.dslist.entities.Game;
+import com.project.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -11,7 +12,6 @@ public class GameMinDTO {
     private String shortDescription;
 
     public GameMinDTO(){
-
     }
 
     public GameMinDTO(Game entity) {
@@ -20,6 +20,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
